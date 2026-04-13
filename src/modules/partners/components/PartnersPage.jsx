8d76cad65e4287fe944a3d76aef22583c8a5a601
Moyosore1605 <moyosorejesuojo@gmail.com>
2026-04-14 00:43:33 +0100

    fixing partner cards

--- src/modules/partners/components/PartnersPage.jsx
+++ src/modules/partners/components/PartnersPage.jsx
@@ -354,7 +354,7 @@ const PartnersPage = () => {
                         </div>
 
                         {/* Bottom 2 centred */}
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                             {PARTNERSHIP_LANES.slice(3).map(lane => (
                                 <PartnershipCard key={lane.id} {...lane} />
                             ))}
